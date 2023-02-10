ffe4ca1aa42e92ecf04fd0cc81671c45046e1ccd
MishynaIra <gortenzia1987@gmail.com>
2023-02-10 17:54:08 +0200

    dispatch вызываем

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
 import {ContactForm} from './ContactForm/ContactForm';
 import {ContactList} from './ContactList/ContatctList';
 import { ContactFilter } from './ContactFilter/ContactFilter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getContactsThunk } from '../redux/contacts/contactThunk';
 
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
   return (
     <>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactReducer, contactsReducer } from './contacts/contactsSlice';
-import { filterReducer } from './contacts/filterSlice';
-import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { contactsReducer } from './contacts/contactsSlice';
+// import { filterReducer } from './contacts/filterSlice';
+// import storage from 'redux-persist/lib/storage';
+// import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 
 // const persistConfig = {
